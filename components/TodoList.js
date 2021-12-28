0de1c0bff7568c19a55f4bab21cd83929d83a2b5
gongdo <inyeoasdf@gmail.com>
2021-12-29 01:14:09 +0900

    first commit

---
+++ components/TodoList.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import TodoItem from './TodoItem';
+
+const TodoList = ({ item }) => {
+  useEffect(() => {
+    console.log(item);
+  }, [item]);
+
+  return (
+    <FlatList
+      style={{ flex: 1 }}
+      data={item}
+      renderItem={({ item }) => <TodoItem text={item} />}
+      keyExtractor={a => a}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    backgroundColor: '#ccc',
+    height: 1,
+  },
+});
+
+export default TodoList;
