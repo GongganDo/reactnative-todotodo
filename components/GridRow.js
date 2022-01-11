47cc3ea4c6c82ba6268f6e6356c0ebfa9f56539d
gongdo <inyeoasdf@gmail.com>
2022-01-12 01:12:50 +0900

    [2048] key 세팅

--- components/GridRow.js
+++ components/GridRow.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import GridBox from './GridBox';
 
-const GridRow = ({ boxList = [] }) => {
+const GridRow = ({ id = 0, boxList = [] }) => {
+    const randomCode = () => Math.random().toString(16).slice(4);
     return (
         <View style={styles.container}>
             {boxList.map(d => (
-                <GridBox num={d} />
+                <GridBox num={d} key={`gridbox_key_${randomCode()}`} />
             ))}
         </View>
     );
