5611ace50181e45cd32cb94d92cb0fcc463be16c
gongdo <inyeoasdf@gmail.com>
2022-01-26 23:52:31 +0900

    [2048] config threshold 수정

--- App.js
+++ App.js
@@ -60,9 +60,9 @@ const App = () => {
                 onSwipe={onSwipe}
                 config={{
                     // swipe가 적용되기 위한 최소 속도
-                    velocityThreshold: 0.05,
+                    velocityThreshold: 0.01,
                     // swipe가 적용되기 위한 최대 거리
-                    directionalOffsetThreshold: 100,
+                    directionalOffsetThreshold: 50,
                 }}
                 style={styles.gestureContainer}>
                 <Grid action={action} />
